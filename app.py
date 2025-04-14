9ee2f58c94d71c0ead3306dd87dc276bea1e9b19
Ammar-Pendroy <ammar.muh313@gmail.com>
2025-04-14 16:30:45 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
+import streamlit as st
+from pages import Home, Menu, Mood_Tracker, Loyalty
+
+st.set_page_config(page_title="Dopamine Coffee ☕", layout="wide")
+
+def main():
+    st.sidebar.title("Navigation")
+    page = st.sidebar.radio("Go to", ["Home", "Menu", "Mood Tracker", "Loyalty"])
+    
+    if page == "Home":
+        Home.show()
+    elif page == "Menu":
+        Menu.show()
+    elif page == "Mood Tracker":
+        Mood_Tracker.show()
+    elif page == "Loyalty":
+        Loyalty.show()
+
+if __name__ == '__main__':
+    main()
 
